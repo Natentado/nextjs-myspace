ad5d2e95ef27b69ce1642541099fbc147caf0d9d
itsjustnatemolina@gmail.com <itsjustnatemolina@gmail.com>
2023-11-16 21:16:06 -0300

    feat: dashboard form implemented

---
+++ nextjs-space/app/dashboard/ProfileForm.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { FormEvent } from "react";
+
+const ProfileForm = ({user}: any) => {
+    
+    const updateUser = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.currentTarget);
+        const body = {
+            name: formData.get("name"),
+            bio: formData.get("bio"),
+            age: formData.get("age"),
+            image: formData.get("image"),
+        };
+
+        const res = await fetch("/api/user", {
+            method: "PUT",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        });
+
+        await res.json();
+    };
+
+    return (
+        <div>
+            <h2>Edit your profile</h2>
+            <form onSubmit={updateUser}>
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" defaultValue={user?.name??""} />
+                <label htmlFor="bio">Bio</label>
+                <textarea 
+                    name="bio"
+                    cols={30}
+                    rows={10}
+                    defaultValue={user?.bio??""}
+                />
+                <label htmlFor="age">Age</label>
+                <input type="text" name="age" defaultValue={user?.age??0} />
+                <label htmlFor="image">Profile image URL</label>
+                <input type="text" name="image" defaultValue={user?.image??""} />
+
+                <button type="submit">Save</button>
+            </form>
+        </div>
+    )
+};
+
+export default ProfileForm;
--- nextjs-space/app/dashboard/page.tsx
+++ nextjs-space/app/dashboard/page.tsx
@@ -1,17 +1,24 @@
 import style from "./style.module.scss";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { prisma } from "@/lib/prisma";
+import ProfileForm from "./ProfileForm";
 
 const Dashboard = async () => {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
 
     if(!session){
         redirect("/api/auth/signin");
     };
 
+    const currentUserEmail = session.user?.email!;
+    const user = prisma.user.findUnique({where: {email: currentUserEmail}});
+
     return (
         <main>
-            Dashboard
+            <h1>Dashboard</h1>
+            <ProfileForm user={user} />
         </main>
     )
 };
